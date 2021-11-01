ce156700801709ba9257193464dd473b0819f073
guneh <gunehee6566@gmail.com>
2021-11-01 10:03:43 -0700

    authentication done

--- app/src/main/java/ca/bcit/park_tamai/MainActivity.java
+++ app/src/main/java/ca/bcit/park_tamai/MainActivity.java
@@ -2,8 +2,12 @@ package ca.bcit.park_tamai;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,11 +15,23 @@ public class MainActivity extends AppCompatActivity {
     private Button btn_ha;
     private Button btn_gender;
     private Button btn_age;
+    private Button btnLogout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnLogout = findViewById(R.id.btnLogout);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut(); // logout
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                finish();
+            }
+        });
 
     }
 }
